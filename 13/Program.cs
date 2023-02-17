3274546f47f5dca8db5320963b3e69fb0a8a64a0
Androooin <Androooin@yandex.ru>
2023-02-17 10:56:37 +0300

    начало

---
+++ 13/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+//сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+Console.WriteLine("Введите целое  число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 99)
+{
+    if (num < 1000)
+    {
+        int result = num % 10;
+        Console.WriteLine(result);
+    }
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+if (num > 999)
+{
+    if (num < 10000)
+    {
+        int result = num % 100 / 10;
+        Console.WriteLine(result);
+
+    }
+}
+if (num > 9999)
+{
+    if (num < 100000)
+    {
+        int result = num % 1000 / 100;
+        Console.WriteLine(result);
+
+    }
+}
+if (num > 99999)
+{
+    if (num < 1000000)
+    {
+        int result = num % 10000 / 100 % 10;
+        Console.WriteLine(result);
+
+    }
+}
+if (num > 999999)
+{
+    Console.WriteLine("Вы ввели слишком большое число");
+}
---
+++ 15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+Console.WriteLine("введите целое число от 1 до 10");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 7)
+{
+Console.WriteLine("Число болшее чем день недели");
+}
+else if (num == 6)Console.WriteLine("да, выходной");
+else if(num == 7 ) Console.WriteLine("да, выходной");
+else Console.WriteLine("нет, будни");
+
+
